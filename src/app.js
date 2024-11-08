150304f94b889de96d04a7379c280f6774dbbdab
ukiratux <131943676+ukiratux@users.noreply.github.com>
2024-11-08 22:44:22 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,17 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Redirect root to /login.html
+app.get('/', (req, res) => {
+  res.redirect('/login.html');
+});
+
+// Serve index.html at /home
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use('/tasks', taskRouter);
 app.use('/statuses', statusRouter);
 app.use('/persons', personRouter);
